512d5e0d579eb91f6c4be9525c2e4776829bbf91
Vlad Burnaev <vlad.burn325@gmail.com>
2020-10-21 16:34:34 +0300

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores doloremque ducimus exercitationem itaque libero possimus rem reprehenderit velit veritatis?</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
